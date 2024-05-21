340c681b34f331a716225444012d5a898a6ba665
unknown <felinhares06@gmail.com>
2024-05-20 23:21:03 -0300

    Comitando as alterações do desafio de projeto

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -1,7 +1,11 @@
-from typing import List
+import pymongo
+
+from typing import List, Optional
 from uuid import UUID
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
-import pymongo
+from datetime import datetime
+
+
 from store.db.mongo import db_client
 from store.models.product import ProductModel
 from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
@@ -28,15 +32,28 @@ class ProductUsecase:
 
         return ProductOut(**result)
 
-    async def query(self) -> List[ProductOut]:
-        return [ProductOut(**item) async for item in self.collection.find()]
+    async def query(self, min_price: Optional[float] = None, max_price: Optional[float] = None) -> List[ProductOut]:
+
+        query_filter = {}
+        if min_price is not None and max_price is not None:
+            query_filter = {"price": {"$gte": min_price, "$lte": max_price}}
+        elif min_price is not None:
+            query_filter = {"price": {"$gte": min_price}}
+        elif max_price is not None:
+            query_filter = {"price": {"$lte": max_price}}
+
+        return [ProductOut(**item) async for item in self.collection.find(query_filter)]
 
     async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
+
         result = await self.collection.find_one_and_update(
             filter={"id": id},
             update={"$set": body.model_dump(exclude_none=True)},
             return_document=pymongo.ReturnDocument.AFTER,
         )
+        
+        if not result:
+            raise NotFoundException(message=f"Product not found with id: {id}")
 
         return ProductUpdateOut(**result)
 
